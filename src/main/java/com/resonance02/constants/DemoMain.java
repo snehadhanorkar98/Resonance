589f2f25256ef8293e7b278b3f1dbef2753bf912
snehadhan98 <sneha.dhanorkar@gmail.com>
2022-05-24 16:50:44 +0530

    update again

--- src/main/java/com/resonance02/constants/DemoMain.java
+++
@@ -1,7 +0,0 @@
-package com.resonance02.constants;
-
-public class DemoMain {
-public static void main(String[] args) {
-	System.out.println("Git push");
-}
-}
---
+++ src/test/java/com/regression/resonance02/DemoTest.java
@@ -0,0 +1,7 @@
+package com.regression.resonance02;
+
+public class DemoTest {
+public static void main(String[] args) {
+	System.out.println("hey....");
+}
+}
