e11411220962169dc988aa73d488bbd7d6b0eea1
snehadhan98 <sneha.dhanorkar@gmail.com>
2022-05-24 15:32:06 +0530

    updated project on branch b1

---
+++ src/test/java/com/regression/resonance02/Regression.java
@@ -0,0 +1,11 @@
+package com.regression.resonance02;
+
+public class Regression {
+	public static void print() {
+		System.out.println("Hi... THis is a demo");
+	}
+
+	public static void main(String[] args) {
+		print();
+	}
+}
