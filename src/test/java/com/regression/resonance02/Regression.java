44ea7c11451ba80ab170050a3eb063d02023ce6a
snehadhan98 <sneha.dhanorkar@gmail.com>
2022-05-24 15:59:18 +0530

    add this code

--- src/test/java/com/regression/resonance02/Regression.java
+++ src/test/java/com/regression/resonance02/Regression.java
@@ -3,6 +3,7 @@ package com.regression.resonance02;
 public class Regression {
 	public static void print() {
 		System.out.println("Hi... THis is a demo");
+		System.out.println("hola....");
 	}
 
 	public static void main(String[] args) {
